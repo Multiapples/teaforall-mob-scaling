5d7071c7612828897655dd0062f94960984b320e
Multiapples <andyhelloandy@gmail.com>
2024-06-29 00:04:16 -0400

    Update nether DoT

--- src/main/java/io/github/multiapples/mixin/PlayerEntityMixin.java
+++ src/main/java/io/github/multiapples/mixin/PlayerEntityMixin.java
@@ -5,7 +5,10 @@ import io.github.multiapples.MobScaling;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,15 +20,14 @@ public abstract class PlayerEntityMixin {
     @Inject(method = "Lnet/minecraft/entity/player/PlayerEntity;tick()V", at = @At("TAIL"))
     protected void tick(CallbackInfo ci) {
         PlayerEntity that = (PlayerEntity)(Object)this;
-        if (that.getEntityWorld().getRegistryKey() == World.NETHER) {
-            if (that.getPos().getY() >= TeaForAllMobScaling.config.netherDOTYlevel
-                    && !MobScaling.getFlagDownedEnderDragon(that.getScoreboard())) {
-
-                boolean success = that.addStatusEffect(new StatusEffectInstance(StatusEffects.WITHER,
-                        60, 1, false, true, true));
-                if (success) {
-                    that.sendMessage(Text.of("Somewhere, a Dragon forbids your presence..."), true);
-                }
+        if (TeaForAllMobScaling.config.netherDOT
+                && that.getEntityWorld().getRegistryKey() == World.NETHER
+                && that.getPos().getY() >= TeaForAllMobScaling.config.netherDOTYlevel
+                && !MobScaling.getFlagDownedEnderDragon(that.getScoreboard())) {
+            boolean success = that.addStatusEffect(new StatusEffectInstance(StatusEffects.WITHER,
+                    60, 1, false, true, true));
+            if (success) {
+                that.sendMessage(Text.of("Somewhere, a Dragon forbids you..."), true);
             }
         }
     }
