980eb14ec487865f1e91d76256d0179d2692c9d8
Multiapples <andyhelloandy@gmail.com>
2024-06-29 13:58:48 -0400

    try catch scaling function

--- src/main/java/io/github/multiapples/mixin/MobEntityMixin.java
+++ src/main/java/io/github/multiapples/mixin/MobEntityMixin.java
@@ -1,5 +1,6 @@
 package io.github.multiapples.mixin;
 
+import com.mojang.logging.LogUtils;
 import io.github.multiapples.MobScaling;
 import net.minecraft.entity.EntityData;
 import net.minecraft.entity.SpawnReason;
@@ -10,12 +11,14 @@ import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MobEntity.class)
 public abstract class MobEntityMixin {
+    private static final Logger LOGGER = LogUtils.getLogger();
     @Inject(method = "Lnet/minecraft/entity/mob/MobEntity;initialize(Lnet/minecraft/world/ServerWorldAccess;Lnet/minecraft/world/LocalDifficulty;Lnet/minecraft/entity/SpawnReason;Lnet/minecraft/entity/EntityData;)Lnet/minecraft/entity/EntityData;",
             at = @At("RETURN"))
     protected void initialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason,
@@ -23,6 +26,11 @@ public abstract class MobEntityMixin {
         // This method gets injected into MobEntity#initialize() after it initializes the mob and right before
         // it returns entityData.
         MobEntity that = (MobEntity)(Object)this;
-        MobScaling.assignMobScaling(that);
+        try {
+            MobScaling.assignMobScaling(that);
+        } catch (Exception e) {
+            LOGGER.error("Error caught while scaling mob.");
+            e.printStackTrace();
+        }
     }
 }
